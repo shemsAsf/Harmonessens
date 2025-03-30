e9802824c2b7fc8510ad1b100de58e7ad5587976
shemsAsf <137190111+shemsAsf@users.noreply.github.com>
2025-03-30 10:19:45 +0200

    Very bad commit, never do that
    Deleting database so render update them with right format

--- backend/config/db.js
+++ backend/config/db.js
@@ -17,41 +17,15 @@ catch {
 // Function to create tables if they don’t exist
 const checkAndCreateTables = () => {
   const createClientsTableQuery = `
-    CREATE TABLE IF NOT EXISTS clients (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      first_name TEXT NOT NULL,
-      last_name TEXT NOT NULL,
-      email TEXT UNIQUE NOT NULL,
-      phone TEXT DEFAULT NULL
-    );
+    DROP TABLE IF EXISTS clients;
   `;
 
   const createAppointmentsTableQuery = `
-    CREATE TABLE IF NOT EXISTS appointments (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      appointmentId INTEGER NOT NULL,
-      start_time TEXT NOT NULL,  -- Stored as ISO 8601 string (e.g., '2025-02-24T14:00:00Z')
-      end_time TEXT NOT NULL,
-      comment TEXT DEFAULT NULL,
-      has_paid INTEGER DEFAULT 0,
-      online INTEGER DEFAULT 0, -- 0 for in-person, 1 for online
-      client_id INTEGER NOT NULL,
-      FOREIGN KEY (appointmentId) REFERENCES services(id) ON DELETE CASCADE,
-      FOREIGN KEY (client_id) REFERENCES clients(id) ON DELETE CASCADE
-    );
+    DROP TABLE IF EXISTS appointments;
   `;
 
   const createServicesTableQuery = `
-    CREATE TABLE IF NOT EXISTS services (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      title TEXT NOT NULL,
-      image TEXT,
-      description TEXT NOT NULL,
-      length INTEGER NOT NULL,
-      price INTEGER NOT NULL,
-      allowOnline INTEGER NOT NULL DEFAULT 0,
-      isActive INTEGER NOT NULL DEFAULT 1
-    );
+    DROP TABLE IF EXISTS services;
   `;
 
   try {
