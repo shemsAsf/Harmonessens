064ef603f81c6cdf83448bfb29ae324f46574424
shemsasfouri <shems.asfouri@gmail.com>
2026-01-14 19:08:25 +0100

    Removing default db auth

--- backend/config/db.js
+++ backend/config/db.js
@@ -17,10 +17,10 @@ if (process.env.NODE_ENV === "test") {
     console.log("Using MySQL test database");
 } else {
     db = mysql.createPool({
-        host: process.env.DB_HOST || "localhost",
-        user: process.env.DB_USER || "root",
-        password: process.env.DB_PASS || "",
-        database: process.env.DB_NAME || "harmonessens",
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
         waitForConnections: true,
         connectionLimit: 10,
         queueLimit: 0,
