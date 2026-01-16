afe5a6ad3d7276bb8ebf2faa2926ae3cae52fa13
shemsasfouri <shems.asfouri@gmail.com>
2026-01-16 16:12:13 +0100

    Update adminController.js

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,8 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const login = (req, res) => {
-  const { password } = req.body;
-  console.log("I'm here!", password);
+  const { password } = req.body;s
 
   if (password !== process.env.ADMIN_PASSWORD) {
     return res.status(401).json({ error: "Unauthorized" });
@@ -16,7 +15,10 @@ const checkEnv = (req, res) => {
   return res.status(404).json({
     adPass : process.env.ADMIN_PASSWORD || "not found",
     email: process.env.EMAIL_USER || "not found",
-    dbHost: process.env.DB_HOST || "not found"
+    dbHost: process.env.DB_HOST || "not found",
+    dbPass: process.env.DB_PASS || "not found",
+    dbUser: process.env.DB_USER || "not found",
+
   })
 }
 
