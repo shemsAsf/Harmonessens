d70b63ae9973d0d0256310c6b9dcd104cbc151db
shemsasfouri <shems.asfouri@gmail.com>
2026-01-18 14:55:32 +0100

    Revert hardcoding host name

--- backend/config/db.js
+++ backend/config/db.js
@@ -17,16 +17,14 @@ if (process.env.NODE_ENV === "test") {
     console.log("Using MySQL test database");
 } else {
     console.log(
-        "host:", process.env.DB_HOST || "localhost",
+        "host:", process.env.DB_HOST,
         "user:", process.env.DB_USER,
         "password:", process.env.DB_PASS,
         "database:", process.env.DB_NAME,
     )
 
-
-
     db = mysql.createPool({
-        host: "localhost",
+        host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
         database: process.env.DB_NAME,
