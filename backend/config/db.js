fafad961e5957913305930790b9f09464c17d8e8
shemsasfouri <shems.asfouri@gmail.com>
2026-01-18 14:07:52 +0100

    Hardcoding db host

--- backend/config/db.js
+++ backend/config/db.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === "test") {
 
 
     db = mysql.createPool({
-        host: process.env.DB_HOST || "localhost",
+        host: "localhost",
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
         database: process.env.DB_NAME,
